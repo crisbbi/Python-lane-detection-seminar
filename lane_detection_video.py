db02607579f04c5db39f0e2742acb4f532ee8cf3
Christian Biasi <christianbiasi@gmx.de>
2019-01-26 18:26:42 +0100

    one hough line for naive method

--- lane_detection_video.py
+++ lane_detection_video.py
@@ -32,48 +32,6 @@ while True:
                 cv2.line(frame, (x1, y1),(x2, y2),(0,255,0),2)
     except:
         pass
-    
-    # hough on masked canny
-    houghLine = cv2.HoughLinesP(blurredMaskedCanny, 1, np.pi/180, 1, np.array([]), 32, 50)
-    try:
-        for x1, y1, x2, y2 in houghLine[0]:
-            maxOfX = max(x1, x2)
-            minOfX = min(x1, x2)
-            maxOfY = max(y1, y2)
-            minOfY = min(y1, y2)
-            slope = abs((maxOfY - minOfY) / (maxOfX - minOfX))
-            if slope > 0.15 and slope < math.inf:
-                cv2.line(frame, (x1, y1),(x2, y2),(0,255,0),2)
-    except:
-        pass
-    
-    # hough on masked canny
-    houghLine = cv2.HoughLinesP(blurredMaskedCanny, 1, np.pi/180, 1, np.array([]), 16, 25)
-    try:
-        for x1, y1, x2, y2 in houghLine[0]:
-            maxOfX = max(x1, x2)
-            minOfX = min(x1, x2)
-            maxOfY = max(y1, y2)
-            minOfY = min(y1, y2)
-            slope = abs((maxOfY - minOfY) / (maxOfX - minOfX))
-            if slope > 0.15 and slope < math.inf:
-                cv2.line(frame, (x1, y1),(x2, y2),(0,255,0),2)
-    except:
-        pass
-    
-    # hough on masked canny
-    houghLine = cv2.HoughLinesP(blurredMaskedCanny, 1, np.pi/180, 1, np.array([]), 8, 12)
-    try:
-        for x1, y1, x2, y2 in houghLine[0]:
-            maxOfX = max(x1, x2)
-            minOfX = min(x1, x2)
-            maxOfY = max(y1, y2)
-            minOfY = min(y1, y2)
-            slope = abs((maxOfY - minOfY) / (maxOfX - minOfX))
-            if slope > 0.15 and slope < math.inf:
-                cv2.line(frame, (x1, y1),(x2, y2),(0,255,0),2)
-    except:
-        pass
 
     #if not ret:
     #    video = cv2.VideoCapture("Lane_detectionVideo_beginEndCut.mp4")
