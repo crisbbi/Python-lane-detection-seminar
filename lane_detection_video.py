1b41d26b6416d6c7742353f36ba40d68d4606e86
Christian Biasi <christianbiasi@gmx.de>
2019-01-27 20:28:12 +0100

    hough improved, threshold increased

--- lane_detection_video.py
+++ lane_detection_video.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-video = cv2.VideoCapture("Driving on a windy rural road_save_road_faded_marks.mp4")
+video = cv2.VideoCapture("Driving on a windy rural road_faded_marks.mp4")
 
 while True:
     ret, frame = video.read()
@@ -19,25 +19,27 @@ while True:
     #pointsOfInterest = np.array([frame[:,240:]])
     #cv2.fillPoly(mask, canny[:,240:], 255)
     maskedCanny = cv2.bitwise_and(canny,mask)
-    blurredMaskedCanny = cv2.GaussianBlur(maskedCanny, (5,5),0)
+    #blurredMaskedCanny = cv2.GaussianBlur(maskedCanny, (5,5),0)
 
     # hough on masked canny
-    houghLine = cv2.HoughLinesP(blurredMaskedCanny, 1, np.pi/180, 1, np.array([]), 64, 100)
+    houghLine = cv2.HoughLinesP(maskedCanny, 1, np.pi/180, 40, np.array([]), 30, 100)
     try:
         for x1, y1, x2, y2 in houghLine[0]:
+            """
             maxOfX = max(x1, x2)
             minOfX = min(x1, x2)
             maxOfY = max(y1, y2)
             minOfY = min(y1, y2)
             slope = abs((maxOfY - minOfY) / (maxOfX - minOfX))
             if slope > 0.15 and slope < math.inf:
-                cv2.line(frame, (x1, y1),(x2, y2),(0,255,0),2)
+                """
+            cv2.line(frame, (x1, y1),(x2, y2),(0,255,0),2)
     except:
         pass
 
     #plt.imshow(frame)
     #plt.show()
-    cv2.imshow("blurry canny", blurredMaskedCanny)
+    cv2.imshow("blurry canny", maskedCanny)
     cv2.imshow("hough", frame)
 
     key = cv2.waitKey(25)
