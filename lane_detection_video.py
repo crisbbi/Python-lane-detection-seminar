18ca0443461c1d2c02d3b28e4a62064c1edbdca0
Christian Biasi <christianbiasi@gmx.de>
2019-01-29 00:57:56 +0100

    houghlines as much improved as i could get it

--- lane_detection_video.py
+++ lane_detection_video.py
@@ -10,7 +10,7 @@ while True:
 
     if ret:
         grayVideo = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        canny = cv2.Canny(grayVideo, 20, 255)
+        canny = cv2.Canny(grayVideo, 10, 255)
     else: break
 
     #put mask on canny
@@ -18,10 +18,10 @@ while True:
     pointsOfInterest = np.array([[300,frame.shape[0] - 160],[505,475],[720,475],[870,frame.shape[0] - 160],[620,520],[550,610],[400,610]])
     cv2.fillPoly(mask, [pointsOfInterest], 255)
     maskedCanny = cv2.bitwise_and(canny,mask)
-    blurredMaskedCanny = cv2.GaussianBlur(maskedCanny, (5,5),0)
+    #blurredMaskedCanny = cv2.GaussianBlur(maskedCanny, (5,5),0)
 
     # hough on masked canny
-    houghLine = cv2.HoughLinesP(blurredMaskedCanny, 1, np.pi/180, 1, np.array([]), 32, 200)
+    houghLine = cv2.HoughLinesP(maskedCanny, 1, np.pi/180, 9, np.array([]), 8, 200)
     try:
         for x1, y1, x2, y2 in houghLine[0]:
             maxOfX = max(x1, x2)
@@ -29,14 +29,14 @@ while True:
             maxOfY = max(y1, y2)
             minOfY = min(y1, y2)
             slope = abs((maxOfY - minOfY) / (maxOfX - minOfX))
-            if slope > 0.4 and slope < math.inf:
+            if slope > 0.25 and slope < math.inf:
                 cv2.line(frame, (x1, y1),(x2, y2),(0,255,0),2)
     except:
         pass
 
     #plt.imshow(frame)
     #plt.show()
-    cv2.imshow("blurry canny", blurredMaskedCanny)
+    cv2.imshow("canny", maskedCanny)
     cv2.imshow("hough", frame)
 
     key = cv2.waitKey(25)
